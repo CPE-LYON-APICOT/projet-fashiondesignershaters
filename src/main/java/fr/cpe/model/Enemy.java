637ffb06097ae993fbd7fa5ddff6043d1adcee40
LenitoO <l.goudjil@eleve.leschartreux.net>
2026-03-31 17:40:18 +0200

    Services

--- src/main/java/fr/cpe/model/Enemy.java
+++ src/main/java/fr/cpe/model/Enemy.java
@@ -1,4 +1,20 @@
 package fr.cpe.model;
 
 public class Enemy {
+
+    public double pv;
+    public double speed;
+    public double position;
+    public double damage;
+
+    public Enemy(double pv, double speed, double position, double damage) {
+        this.pv = pv;
+        this.speed = speed;
+        this.position = position;
+        this.damage = damage;
+    }
+
+    public void move(){
+
+    }
 }
